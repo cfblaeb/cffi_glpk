7a05fdbf0256c1750106fb292f36d48fa9f13f5f
cfblaeb <laeb@dtu.dk>
2020-10-17 23:27:16 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='cffi_glpk',
-    version='0.4',
+    version='0.5',
     url='https://github.com/cfblaeb/cffi_glpk',
     author='laeb',
     author_email='laeb@dtu.dk',
     description='CFFI bindings for GLPK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
 
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["cffi_glpk/cffi_glpk_build.py:ffibuilder"],
